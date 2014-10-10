50fe143fc9001f6e712527078eaac21fe241ff66
Nathan Witmer <nathan@zerowidth.com>
2014-10-10 15:01:31 -0600

    Evaluate results for mismatched observations

--- lib/scientist/result.rb
+++ lib/scientist/result.rb
@@ -7,15 +7,36 @@ class Scientist::Result
   # An Array of Observations in execution order.
   attr_reader :observations
 
-  # The String name of the primary observed behavior.
+  # The primary Observation
   attr_reader :primary
+
+  # An Array of observations which didn't match the primary
+  attr_reader :mismatched
 
   # Internal: Create a new result.
   def initialize(experiment, observations:, primary:)
-    @experiment = experiment
+    @experiment   = experiment
     @observations = observations
-    @primary = primary
+    @primary      = primary
+    @mismatched   = evaluate_observations_for_mismatches
 
     freeze
+  end
+
+  # Public: was the result a match between all behaviors?
+  def match?
+    mismatched.empty?
+  end
+
+  # Public: were there mismatches in the behaviors?
+  def mismatch?
+    mismatched.any?
+  end
+
+  # Internal: evaluate the observations to determine if the observations match.
+  def evaluate_observations_for_mismatches
+    observations.reject do |observation|
+      observation == primary || primary.equivalent_to?(observation)
+    end
   end
 end
--- test/scientist/result_test.rb
+++ test/scientist/result_test.rb
@@ -8,4 +8,31 @@ describe Scientist::Result do
 
     assert result.frozen?
   end
+
+  it "evaluates its observations" do
+    a = Scientist::Observation.new("a") { 1 }
+    b = Scientist::Observation.new("b") { 1 }
+
+    assert a.equivalent_to?(b)
+
+    result = Scientist::Result.new nil, observations: [a, b], primary: a
+    assert result.match?
+    refute result.mismatch?
+    assert_equal [], result.mismatched
+
+    x = Scientist::Observation.new("x") { 1 }
+    y = Scientist::Observation.new("y") { 2 }
+    z = Scientist::Observation.new("z") { 3 }
+
+    result = Scientist::Result.new nil, observations: [x, y, z], primary: x
+    refute result.match?
+    assert result.mismatch?
+    assert_equal [y, z], result.mismatched
+  end
+
+  it "has no mismatches if there is only a primary observation" do
+    a = Scientist::Observation.new("a") { 1 }
+    result = Scientist::Result.new nil, observations: [a], primary: a
+    assert result.match?
+  end
 end
